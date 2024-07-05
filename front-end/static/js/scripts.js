a5ce8dbdd09ce1ffc2f056b9ca13113b1b2d63d2
afardan <lymfrdat@gmail.com>
2024-07-05 15:15:05 +0300

    the css is being removed but not added for the likes and dislikes of the post needs some fixes

--- front-end/static/js/scripts.js
+++ front-end/static/js/scripts.js
@@ -51,19 +51,8 @@ function registerAction() {
     });
 }
 
-{/* <form action="/likeOrDislikePost" method="post" class="d-inline">
-<input type="hidden" name="postID" value="{{$post.ID}}">
-<input type="hidden" name="isLike" value="like">
-<button type="submit" class="btn btn-outline-success btn-sm
-                {{if $post.Like.IsLiked}}
-                custom-hover-like
-                {{end}}">
-    <i class="bi bi-hand-thumbs-up"></i>
-</button>
-</form> */}
-
 function likeDislikePost(postID, isLike) {
-    event.preventDefault(); //  the default form submission
+    event.preventDefault(); // Prevent the default form submission
     $.ajax({
         url: "/likeOrDislikePost",
         type: "POST",
@@ -73,9 +62,23 @@ function likeDislikePost(postID, isLike) {
         }),
         contentType: "application/json",
         success: function (data) {
+            // Assuming the server response contains the updated like/dislike status
+            // data should include fields like data.isLiked and data.isDisliked
+            const likeBtn = document.getElementById('like-btn-' + postID);
+            const dislikeBtn = document.getElementById('dislike-btn-' + postID);
+
+            if (data.isLiked) {
+                likeBtn.classList.add('custom-hover-like');
+                dislikeBtn.classList.remove('custom-hover-dislike');
+            } else if (data.isDisliked) {
+                likeBtn.classList.remove('custom-hover-like');
+                dislikeBtn.classList.add('custom-hover-dislike');
+            } else {
+                likeBtn.classList.remove('custom-hover-like');
+                dislikeBtn.classList.remove('custom-hover-dislike');
+            }
         },
         error: function (data) {
-
             alert(data.responseText);
             $("#error").html(data.responseText); // Assuming your server sends plain text error messages
             $("#error").show();
@@ -84,6 +87,7 @@ function likeDislikePost(postID, isLike) {
         }
     });
 }
+
 
 
 function likeDislikeComment(commentID, isLike) {
